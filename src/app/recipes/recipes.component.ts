55f4a153709b430104fec1481d2bdd62b236e0b5
Atikom Juntakool <Tiki@Atikoms-MacBook-Pro.local>
2017-02-05 22:01:20 -0800

    All files.

---
+++ src/app/recipes/recipes.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+// import { Recipe } from './recipe';
+// import { RecipeItemComponent } from './recipe-list/recipe-item.component';
+// import { RecipeListComponent } from './recipe-list/recipe-list.component';
+
+@Component({
+  selector: 'rb-recipes',
+  templateUrl: './recipes.component.html'
+})
+export class RecipesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+}
